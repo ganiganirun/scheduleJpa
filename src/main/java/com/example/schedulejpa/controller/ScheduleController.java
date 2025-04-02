cf21b0c683666f2e8597ad719fb609c6d6c270c7
songgaeun <rkdmsdmqek1128@gmail.com>
2025-04-02 16:15:29 +0900

    feat: ScheduelController class post 기능 추가

---
+++ src/main/java/com/example/schedulejpa/controller/ScheduleController.java
@@ -0,0 +1,34 @@
+package com.example.schedulejpa.controller;
+
+import com.example.schedulejpa.dto.ScheduleResponseDto;
+import com.example.schedulejpa.dto.SchelduleRequestDto;
+import com.example.schedulejpa.service.ScheduleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/schedules")
+@RequiredArgsConstructor // final or @NonNull 이 붙은 필드만 매개변수로 받는 생성자 자동 생성 어노테이션
+public class ScheduleController {
+
+  private final ScheduleService scheduleService;
+
+  @PostMapping
+  public ResponseEntity<ScheduleResponseDto> saveSchedule(@RequestBody SchelduleRequestDto requestDto){
+
+    ScheduleResponseDto scheduleResponseDto =
+        scheduleService.saveSchedule(
+            requestDto.getUsername(),
+            requestDto.getTitle(),
+            requestDto.getContents()
+        );
+
+    return new ResponseEntity<>(scheduleResponseDto, HttpStatus.CREATED);
+  }
+
+}
