ee00aa7a9a393f7c4172552de91192cdd980ed78
songgaeun <rkdmsdmqek1128@gmail.com>
2025-04-02 20:45:10 +0900

    docs: spl 파일 수정

--- schedule.sql
+++ schedule.sql
@@ -1,20 +1,28 @@
 CREATE TABLE member(
-    id  BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '고유 식별자',
-    username VARCHAR(100) NOT NULL COMMENT '유저 이름',
-    email VARCHAR(255) NOT NULL UNIQUE COMMENT '유저 이메일',
-    created_at DATETIME COMMENT '생성일',
-    modified_at DATETIME COMMENT '수정일'
+                       id  BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '고유 식별자',
+                       username VARCHAR(100) NOT NULL COMMENT '유저 이름',
+                       email VARCHAR(255) NOT NULL UNIQUE COMMENT '유저 이메일',
+                       created_at DATETIME COMMENT '생성일',
+                       modified_at DATETIME COMMENT '수정일'
 );
-
-ALTER TABLE member ADD COLUMN 'password' VARCHAR(100)
 
 CREATE TABLE schedule(
-    id  BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '고유 식별자',
-    member_id BIGINT NOT NULL COMMENT '와래키',
-    username VARCHAR(100) NOT NULL COMMENT '작성 유저명',
-    title VARCHAR(255) NOT NULL COMMENT '할일 제목',
-    contents LONGTEXT NOT NULL COMMENT '할일 내용',
-    created_at DATETIME COMMENT '생성일',
-    modified_at DATETIME COMMENT '수정일'.
-    FOREIGN KEY (member_id) REFERENCES member(id)
+                         id  BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '고유 식별자',
+                         username VARCHAR(100) NOT NULL COMMENT '작성 유저명',
+                         title VARCHAR(255) NOT NULL COMMENT '할일 제목',
+                         contents LONGTEXT NOT NULL COMMENT '할일 내용',
+                         created_at DATETIME COMMENT '생성일',
+                         modified_at DATETIME COMMENT '수정일'
 );
+
+
+
+
+ALTER TABLE schedule ADD COLUMN member_id BIGINT NOT NULL COMMENT '와래키';
+
+ALTER TABLE schedule ADD CONSTRAINT fk_member FOREIGN KEY (member_id) REFERENCES member(id);
+
+ALTER TABLE schedule DROP COLUMN username;
+
+
+ALTER TABLE member ADD COLUMN password VARCHAR(100)
