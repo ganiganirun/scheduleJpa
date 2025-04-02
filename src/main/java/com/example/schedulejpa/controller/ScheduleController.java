a442a56d0b2cc890df13482e41bcfda70bcaa214
songgaeun <rkdmsdmqek1128@gmail.com>
2025-04-02 17:21:03 +0900

    feat: 스캐줄 컨트롤러 전체 스케줄 조회 기능 구현

--- src/main/java/com/example/schedulejpa/controller/ScheduleController.java
+++ src/main/java/com/example/schedulejpa/controller/ScheduleController.java
@@ -3,9 +3,11 @@ package com.example.schedulejpa.controller;
 import com.example.schedulejpa.dto.ScheduleResponseDto;
 import com.example.schedulejpa.dto.SchelduleRequestDto;
 import com.example.schedulejpa.service.ScheduleService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +32,16 @@ public class ScheduleController {
         );
 
     return new ResponseEntity<>(scheduleResponseDto, HttpStatus.CREATED);
+  }
+
+  // 일정 조회
+  @GetMapping
+  public ResponseEntity<List<ScheduleResponseDto>> findAllSchedule(){
+
+    List<ScheduleResponseDto> scheduleResponseDtoList = scheduleService.findAll();
+
+    return new ResponseEntity<>(scheduleResponseDtoList,HttpStatus.OK);
+
   }
 
 }
