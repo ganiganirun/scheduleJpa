a2f3e3b596a12f503145b3aabfed83693c038caf
songgaeun <rkdmsdmqek1128@gmail.com>
2025-04-02 16:16:05 +0900

    feat: ScheduleRepository class post 기능 추가

---
+++ src/main/java/com/example/schedulejpa/repository/ScheduleRepository.java
@@ -0,0 +1,8 @@
+package com.example.schedulejpa.repository;
+
+import com.example.schedulejpa.entity.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+}
