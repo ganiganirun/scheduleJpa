618e5c084dd3a1e2a389500b5d6d99fbe43e632f
songgaeun <rkdmsdmqek1128@gmail.com>
2025-04-03 17:03:31 +0900

    refactor: 스케줄요청디티오 수정
    유저이름 삭제

--- src/main/java/com/example/schedulejpa/dto/scheduledto/SchelduleRequestDto.java
+++ src/main/java/com/example/schedulejpa/dto/scheduledto/SchelduleRequestDto.java
@@ -5,15 +5,17 @@ import lombok.Getter;
 @Getter
 public class SchelduleRequestDto {
 
-  private final String username;
+//  private final String username;
+
+//  private final Long userId;
 
   private final String title;
 
   private final String contents;
 
   // 필드가 final로 선언되어서 생성자가 필요
-  public SchelduleRequestDto(String username, String title, String contents) {
-    this.username = username;
+  public SchelduleRequestDto(String title, String contents) {
+//    this.userId = userId;
     this.title = title;
     this.contents = contents;
   }
