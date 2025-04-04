4fb59a89dd7b79fc5548082612b7490a208d9eb0
songgaeun <rkdmsdmqek1128@gmail.com>
2025-04-04 11:55:23 +0900

    feat: 멤버리포지토리 로그인을 위한 기능 구현

--- src/main/java/com/example/schedulejpa/repository/MemberRepository.java
+++ src/main/java/com/example/schedulejpa/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package com.example.schedulejpa.repository;
 
 import com.example.schedulejpa.entity.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
@@ -26,6 +27,18 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
             new ResponseStatusException(
                 HttpStatus.NOT_FOUND,
                 "Does not exit id = "+id
+            )
+        );
+  }
+
+  Optional<Member> findIdByEmailAndPassword(String email, String password);
+
+  default Member findIdByEmailAndPasswordOrElseThrow(String email, String password){
+    return findIdByEmailAndPassword(email,password)
+        .orElseThrow(()->
+            new ResponseStatusException(
+                HttpStatus.UNAUTHORIZED,
+                "일치하는 아이디와 비밀번호가 존재하지 않습니다. 다시 확인해주세요."
             )
         );
   }
